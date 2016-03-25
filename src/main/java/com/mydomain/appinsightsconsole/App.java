af62dce1e18238887b00859052cdc5ee9429254a
normalian <warito_hutsu@hotmail.com>
2016-03-25 10:43:00 +0900

    logback code

--- src/main/java/com/mydomain/appinsightsconsole/App.java
+++ src/main/java/com/mydomain/appinsightsconsole/App.java
@@ -1,5 +1,8 @@
 package com.mydomain.appinsightsconsole;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.applicationinsights.TelemetryConfiguration;
 
@@ -8,6 +11,8 @@ import com.microsoft.applicationinsights.TelemetryConfiguration;
  *
  */
 public class App {
+	final static Logger logger = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
 		// Application Insights の設定を有効化
 		TelemetryConfiguration configuration = TelemetryConfiguration.getActive();
@@ -15,6 +20,8 @@ public class App {
 
 		telemetryClient.trackTrace("コンソールアプリからのトレースメッセージ");
 		telemetryClient.trackException(new RuntimeException("コンソールアプリの自作例外"), null, null);
+
+		logger.info("Logback 上からのメッセージ");
 
 		// 最後に flush しないとリクエストがメモリ上に滞留したままアプリが終了する
 		telemetryClient.flush();
